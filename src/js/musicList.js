93bc6d661f2f272a847fe535e51ad8c332be9e5d
tuannvbg <nguyentuan2005@gmail.com>
2020-07-08 16:29:34 +0700

    Update musicList.js

--- src/js/musicList.js
+++ src/js/musicList.js
@@ -4,11 +4,11 @@
     
 var musicList = [
     {
-        title : '如果一切没有发生过',
-        singer : '张碧晨',
-        cover  : 'http://7xonl1.com1.z0.glb.clouddn.com/rYcmv.jpg',
-        src    : 'http://7xonl1.com1.z0.glb.clouddn.com/Cm45K2.mp3',
-		lyric  : "src/data/yqdmfsg.lrc"
+        title : 'Ngắm Hoa Lệ Rơi',
+        singer : 'Châu Khải Phong',
+        cover  : 'https://zmp3-photo.zadn.vn/thumb/240_240/covers/6/9/69f213a075a1ef6b0b042e242c837178_1489986205.png',
+        src    : 'https://dl.dropboxusercontent.com/s/vboepo1elrfaa6p/Ngam-Hoa-Le-Roi-Chau.Khai.Phong.mp3',
+		lyric  : "https://dl.dropboxusercontent.com/s/ecxh7g6zvlh0qmh/Chau.Khai.Phong_Ngam.Hoa.Le.Roi.lrc"
     },
     {
         title : '车站',
@@ -18,11 +18,11 @@ var musicList = [
 		lyric  : "src/data/chezhan.lrc"
     },
     {
-        title : '年轮 ',
-        singer : '张碧晨',
-        cover  : 'http://7xonl1.com1.z0.glb.clouddn.com/1EyrQ3.jpg',
-        src    : 'http://7xonl1.com1.z0.glb.clouddn.com/HpaN71.mp3',
-		lyric  : "src/data/nianlun.lrc"
+        title : 'Cô gái M52',
+        singer : 'HuyR ft Tùng Viu',
+        cover  : 'https://zmp3-photo.zadn.vn/thumb/240_240/covers/f/f/ff44d05771e686143a49b6a73dd844bb_1519265212.jpg',
+        src    : 'https://dl.dropboxusercontent.com/s/apzykhmjjbrnr3l/Co-Gai-M52-HuyRftTungViu.mp3',
+		lyric  : "https://dl.dropboxusercontent.com/s/tna8vn6q0pdraow/HuyR.TungViu_Co.Gai.M52.lrc"
     },
     {
         title : '她说',
