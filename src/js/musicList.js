df82b8a91b91a8245f8275214722ca17c2d1b7ae
tuannvbg <nguyentuan2005@gmail.com>
2020-07-08 16:31:04 +0700

    Create musicList.js

---
+++ src/js/musicList.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Smohan on 2015/5/15.
+ */
+    
+var musicList = [
+    {
+        title : 'Ngắm Hoa Lệ Rơi',
+        singer : 'Châu Khải Phong',
+        cover  : 'https://zmp3-photo.zadn.vn/thumb/240_240/covers/6/9/69f213a075a1ef6b0b042e242c837178_1489986205.png',
+        src    : 'https://dl.dropboxusercontent.com/s/vboepo1elrfaa6p/Ngam-Hoa-Le-Roi-Chau.Khai.Phong.mp3',
+		lyric  : "https://dl.dropboxusercontent.com/s/ecxh7g6zvlh0qmh/Chau.Khai.Phong_Ngam.Hoa.Le.Roi.lrc"
+    },
+    {
+        title : '车站',
+        singer : '李健',
+        cover  : 'http://7xonl1.com1.z0.glb.clouddn.com/1DYuS4.jpg',
+        src    : 'http://7xonl1.com1.z0.glb.clouddn.com/1VvcW3.mp3',
+		lyric  : "src/data/chezhan.lrc"
+    },
+    {
+        title : 'Cô gái M52',
+        singer : 'HuyR ft Tùng Viu',
+        cover  : 'https://zmp3-photo.zadn.vn/thumb/240_240/covers/f/f/ff44d05771e686143a49b6a73dd844bb_1519265212.jpg',
+        src    : 'https://dl.dropboxusercontent.com/s/apzykhmjjbrnr3l/Co-Gai-M52-HuyRftTungViu.mp3',
+		lyric  : "https://dl.dropboxusercontent.com/s/tna8vn6q0pdraow/HuyR.TungViu_Co.Gai.M52.lrc"
+    },
+    {
+        title : '她说',
+        singer : '张碧晨',
+        cover  : 'http://7xonl1.com1.z0.glb.clouddn.com/zUlA31.jpg',
+        src    : 'http://7xonl1.com1.z0.glb.clouddn.com/1O1TO4.mp3',
+		lyric  : null
+    },
+    {
+        title : '赤血长殷',
+        singer : '王凯',
+        cover  : 'http://7xonl1.com1.z0.glb.clouddn.com/eukCI.jpg',
+        src    : 'http://7xonl1.com1.z0.glb.clouddn.com/14hk24.mp3',
+		lyric  : null
+    },
+    {
+        title : 'My Love',
+        singer : 'WestLife',
+        cover  : 'http://7xonl1.com1.z0.glb.clouddn.com/NYBS32.jpg',
+        src    : 'http://7xonl1.com1.z0.glb.clouddn.com/1IdhM4.mp3',
+		lyric  : null
+    }
+];
